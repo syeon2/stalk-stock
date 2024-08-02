75e96f25b7b8ad1816845b765716599f0ce63687
SUYEON KIM <waterkite94@gmail.com>
2024-08-02 15:16:50 +0900

    [Fix] update build.gradlew to manifest class

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -4,3 +4,11 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
+
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
