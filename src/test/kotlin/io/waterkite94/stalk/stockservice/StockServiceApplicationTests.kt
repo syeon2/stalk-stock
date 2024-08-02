8b664e7a421a1e406ce92630196d1f867030ae2a
SUYEON KIM <waterkite94@gmail.com>
2024-08-02 14:44:04 +0900

    Init Stalk Stock Service

---
+++ src/test/kotlin/io/waterkite94/stalk/stockservice/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.waterkite94.stalk.stockservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
