26176d82895f89e694babf1250edbd7f87859e67
SUYEON KIM <waterkite94@gmail.com>
2024-08-02 14:55:42 +0900

    [Chore] set up checkstyle and ktlint
    - Update: build.gradle structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
-    id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm")
+    kotlin("plugin.spring") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint")
 }
-
-group = "io.waterkite94.stalk"
-version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
@@ -14,24 +12,53 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
-}
+val projectGroup: String by project
+val applicationVersion: String by project
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+allprojects {
+    group = projectGroup
+    version = applicationVersion
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    dependencies {
+        // base config
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+        // spring
+        implementation("org.springframework.boot:spring-boot-starter")
+
+        // test
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.getByName("bootJar") {
+        enabled = false
+    }
+
+    tasks.getByName("jar") {
+        enabled = true
+    }
 }
